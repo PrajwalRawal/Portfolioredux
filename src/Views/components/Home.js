7248ab0b9a68b65074e8fa79afc3a9e41c86d385
Ringo007 <ringo@Prajwals-MacBook-Air.local>
2019-07-08 18:01:22 +0545

    Adding FilesL

--- src/Views/components/Home.js
+++ src/Views/components/Home.js
@@ -5,7 +5,62 @@ export default class Home extends Component {
     render() {
         return (
             <MainLayout>
-                <h1>Hello From the other side</h1>
+                <section id="hero">
+                    <h1 className="h5-text">Our Works</h1>
+                    <h2 className="h1-text">We leverage design to give brands purpose.</h2>
+                    <div className="portfolio">
+                        <div className="card">
+                            <div className="image">
+                                <img src="https://demo.layerz.com.ve/horet/assets/img/works/01.jpg" alt="hello"/>
+                            </div>
+                            <div className="detail">
+                                <span className="title">Hello World</span>
+                                <span className="category">Arts</span>
+                                <i className="right"></i>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="image">
+                                <img src="https://demo.layerz.com.ve/horet/assets/img/works/01.jpg" alt="hello"/>
+                            </div>
+                            <div className="detail">
+                                <span className="title">Hello World</span>
+                                <span className="category">Arts</span>
+                                <i className="right"></i>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="image">
+                                <img src="https://demo.layerz.com.ve/horet/assets/img/works/01.jpg" alt="hello"/>
+                            </div>
+                            <div className="detail">
+                                <span className="title">Hello World</span>
+                                <span className="category">Arts</span>
+                                <i className="right"></i>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="image">
+                                <img src="https://demo.layerz.com.ve/horet/assets/img/works/01.jpg" alt="hello"/>
+                            </div>
+                            <div className="detail">
+                                <span className="title">Hello World</span>
+                                <span className="category">Arts</span>
+                                <i className="right"></i>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="image">
+                                <img src="https://demo.layerz.com.ve/horet/assets/img/works/01.jpg" alt="hello"/>
+                            </div>
+                            <div className="detail">
+                                <span className="title">Hello World</span>
+                                <span className="category">Arts</span>
+                                <i className="right"></i>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </MainLayout>
         )
     }
--- src/Views/layouts/MainLayout.js
+++ src/Views/layouts/MainLayout.js
@@ -1,6 +1,11 @@
 import React, { Component, Fragment } from 'react'
+import {Link} from 'react-router-dom'
 
 export default class MainLayout extends Component {
+    handleClick = () => {
+        document.getElementById('menu').classList.toggle('active');
+        document.body.classList.toggle('hidden');
+    }
     render() {
         return (
             <Fragment> 
@@ -9,18 +14,46 @@ export default class MainLayout extends Component {
                     <div className="brand">
                         <img src="https://demo.layerz.com.ve/horet/assets/img/logo.png" alt="Brand"/>
                     </div>
-                    <div className="icon">
-                        <div>
+                    <div className="icon" >
+                        <div onClick={this.handleClick}>
                             <span>Menu</span>
                             <img src="/images/menu.svg"/>
                         </div>
                     </div>
+                    <nav id="menu">
+                        <span onClick={this.handleClick}>X</span>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">About</Link>
+                            </li>
+                            <li>
+                                <Link to="/works">Works</Link>
+                            </li>
+                            <li>
+                                <Link to="/contact">Contact</Link>
+                            </li>
+                        </ul>
+                    </nav>
                 </header>
                 <main>
                     {this.props.children}
                 </main>
                 <footer>
-                    this is footer
+                    <div className="copyright">
+                        &copy; {new Date().getFullYear()}. All Rights Reserved.
+                    </div>
+                    <div className="social">
+                        <ul>
+                            <li><a href="https://facebook.com">Fb</a></li>
+                            <li><a href="https://twitter.com">Tw</a></li>
+                            <li><a href="https://instagram.com">Ig</a></li>
+                            <li><a href="https://Youtube.com">Yt</a></li>
+                            <li><a href="https://LinkedIn.com">In</a></li>
+                        </ul>
+                    </div>
                 </footer>
 
             </Fragment>
